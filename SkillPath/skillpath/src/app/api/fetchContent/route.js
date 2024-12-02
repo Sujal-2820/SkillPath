13aec69a044212a8dcd675e7413ca306e928092c
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2024-12-02 12:26:54 +0530

    Update route.js
    Updated code for secure credentials

--- SkillPath/skillpath/src/app/api/fetchContent/route.js
+++ SkillPath/skillpath/src/app/api/fetchContent/route.js
@@ -3,10 +3,14 @@
 import { Storage } from '@google-cloud/storage';
 import { NextResponse } from 'next/server';
 
+const credential = JSON.parse(process.env.NEXT_PUBLIC_GOOGLE_APPLICATION_CREDENTIALS)
 // Initialize the Google Cloud Storage client
 const storage = new Storage({
   projectId: process.env.NEXT_PUBLIC_GOOGLE_CLOUD_PROJECT,
-  keyFilename: process.env.NEXT_PUBLIC_GOOGLE_APPLICATION_CREDENTIALS,
+  credentials: {
+    client_email: credential.client_email,
+    private_key: credential.private_key,
+  },
 });
 
 export async function GET(request) {
