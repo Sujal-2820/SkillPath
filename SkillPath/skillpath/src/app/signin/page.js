350d7f755741a336f6a3187e2d3a69c50a1e61a3
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2024-12-05 11:28:01 +0530

    Update page.js
    Added necessary tailwind code

--- SkillPath/skillpath/src/app/signin/page.js
+++ SkillPath/skillpath/src/app/signin/page.js
@@ -79,7 +79,7 @@ export default function SignIn() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm text-black"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
@@ -97,7 +97,7 @@ export default function SignIn() {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm text-black"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
