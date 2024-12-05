8c6019a0b87f73241fe8156267f2042e49e389f6
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2024-12-05 11:27:26 +0530

    Update page.js
    Modified Tailwind

--- SkillPath/skillpath/src/app/signup/page.js
+++ SkillPath/skillpath/src/app/signup/page.js
@@ -100,7 +100,7 @@ export default function SignUp() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm text-black"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
@@ -118,7 +118,7 @@ export default function SignUp() {
                   type="password"
                   autoComplete="new-password"
                   required
-                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm text-black"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
